1305a7985d427dd65222286db6eb80c63494a052
Cenkg9 <cenkgy2407@gmail.com>
2026-05-07 22:58:23 +0200

    Remove directUrl from prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,6 +8,5 @@ export default defineConfig({
   },
   datasource: {
     url: process.env["POSTGRES_PRISMA_URL"] ?? process.env["DATABASE_URL"],
-    directUrl: process.env["POSTGRES_URL_NON_POOLING"],
   },
 });
